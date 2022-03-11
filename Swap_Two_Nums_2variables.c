72826427992b4148b359bac5b2cf09fbaa0fedbd
Rohan Sahai Mathur <65111149+rosama74@users.noreply.github.com>
2022-03-11 20:51:50 +0530

    Created Swap_Two_Nums_2variables.c
    A C-Program to perform the swapping of two numbers without using a temporary variable.

---
+++ Swap_Two_Nums_2variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Swapping two numbers without using third variable\nUsing two variables 'a' and 'b'.");
+  printf("\nEnter value for a : ");
+  scanf("%d",&a);
+  printf("\nEnter value for b : ");
+  scanf("%d",&b);
+  printf("\nBefore Swapping\n");
+  printf("a = %d\nb = %d\n",a,b);
+  a = a - b;
+  b = a + b;
+  a = b - a;
+  printf("\nAfter Swapping\n");
+  printf("a = %d\nb = %d\n",a,b);
+  return 0;
+}
