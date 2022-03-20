64502b5137444c9443e91d446c55e76356b07023
Rohan Sahai Mathur <65111149+rosama74@users.noreply.github.com>
2022-03-20 13:03:39 +0530

    Create Is_Prime.c
    To check whether a number is prime or not

---
+++ Is_Prime.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int prime(int num, int i)
+{
+    if(i==1)
+    {
+        return 1;
+    }
+    else
+    {
+        if(num % i == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return prime(num,i-1);
+        }
+    }
+}
+int main()
+{
+    int num,check;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    check = prime(num,num/2);
+    if(check)
+    {
+        printf("%d is a prime number.",num);
+    }
+    else
+    {
+        printf("%d is not a prime number.",num);
+    }
+}
